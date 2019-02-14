6469bba5d39c26923ddbd4566270691007115307
erlanggal <erlangga.laimena@imslogistics.co.id>
2019-02-15 06:36:22 +0700

    Learn how to implementation VAR and Data-Type

--- Declaration-Operator/DeclarationOperator.go
+++ Declaration-Operator/DeclarationOperator.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+//var declare at package level
+var city = "Tangerang"
+
+//assign var with data type
+//variable example is assign with int data type
+//because example is assign with just data type, the value of example is 0(default)
+var example int
+
+//example of variable assign with data type and value
+var example2 int = 100
+
 func main() {
 	//First declaration of new variable must use ":=" operator
 	x := 24
@@ -19,4 +30,14 @@ func main() {
 
 	z := "Erlangga Laimena"
 	fmt.Println(z)
+
+	//var can be declare both on package or function level
+	//var declare at function level
+	var country = "Indonesia"
+	fmt.Println(country)
+
+	//call var that declare in package level
+	fmt.Println(city)
+	fmt.Println(example)
+	fmt.Println(example2)
 }
