6edc137aeb8e6e61e7475f9ec86b7531e15e6769
erlanggal <erlangga.laimena@imslogistics.co.id>
2019-02-15 16:17:45 +0700

    Exercise 2

---
+++ Exercise1.2/Exercise.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+//TASK 1
+//Use var to DECLARE three VARIABLES. The variables should have package level scope.
+//Do not assign VALUES to the variables.
+//Use the following IDENTIFIERS for the variables and make sure the variables
+//are of the following TYPE (meaning they can store VALUES of that TYPE).
+
+//identifier “x” type int
+var x int
+
+//identifier “y” type string
+var y string
+
+//identifier “z” type bool
+var z bool
+
+func main() {
+	//TASK 2
+	//print out the values for each identifier
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
