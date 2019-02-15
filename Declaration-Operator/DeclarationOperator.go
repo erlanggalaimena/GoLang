21d54ada77ad56ee1b490bf099034fceec226ca7
erlanggal <erlangga.laimena@imslogistics.co.id>
2019-02-15 14:39:52 +0700

    create my type in GoLang

--- Declaration-Operator/DeclarationOperator.go
+++ Declaration-Operator/DeclarationOperator.go
@@ -13,6 +13,12 @@ var example int
 //example of variable assign with data type and value
 var example2 int = 100
 
+//create my Data-Type with "type"
+type laimena int
+
+//assign my Data-Type to variable 'example3'
+var example3 laimena
+
 func main() {
 	//First declaration of new variable must use ":=" operator
 	x := 24
@@ -30,6 +36,8 @@ func main() {
 
 	z := "Erlangga Laimena"
 	fmt.Println(z)
+	fmt.Println("------------------")
+	fmt.Println()
 
 	//var can be declare both on package or function level
 	//var declare at function level
@@ -40,8 +48,17 @@ func main() {
 	fmt.Println(city)
 	fmt.Println(example)
 	fmt.Println(example2)
+	fmt.Println("------------------")
+	fmt.Println()
 
 	//print type of variable
 	fmt.Printf("%s %T\n", "Data-Type of variable example is : ", example)
 	fmt.Printf("%s %T\n", "Data-Type of variable example2 is : ", example2)
+	fmt.Println("------------------")
+	fmt.Println()
+
+	//assign value into variable that has my Data-Type
+	example3 = 20
+	fmt.Println(example3)
+	fmt.Printf("%T\n", example3)
 }
