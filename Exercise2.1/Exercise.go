39fca625463e399bf1b2431ff134c6c38e11ddf6
erlanggal <erlangga.laimena@imslogistics.co.id>
2019-02-21 21:33:27 +0700

    exercise 2.1

---
+++ Exercise2.1/Exercise.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	//TASK
+	//Print Decimal, Binary and Hexa Decimal of value
+	x := 18
+	fmt.Println(x)
+	fmt.Printf("Decimal : %d\n", x)
+	fmt.Printf("Binary : %b\n", x)
+	fmt.Printf("Decimal : %#x\n", x)
+}
