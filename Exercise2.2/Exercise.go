90c969dbb35d995283a48d1fcfaa2ef1bc00647d
erlanggal <erlangga.laimena@imslogistics.co.id>
2019-02-21 21:44:02 +0700

    Exercise 2.2

---
+++ Exercise2.2/Exercise.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	//TASK
+	//Using the following operators, write expressions and assign their values to variables
+	//==
+	a := 24
+	b := 18
+	statement1 := (a == b)
+	fmt.Println(statement1)
+	//<=
+	statement1 = (a <= b)
+	fmt.Println(statement1)
+	//>=
+	statement1 = (a >= b)
+	fmt.Println(statement1)
+	//!=
+	statement1 = (a != b)
+	fmt.Println(statement1)
+	//<
+	statement1 = (a < b)
+	fmt.Println(statement1)
+	//>
+	statement1 = (a < b)
+	fmt.Println(statement1)
+}
