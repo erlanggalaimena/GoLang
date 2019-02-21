5ced304ce11ead5370b588db0b1ad3ecc27f0b63
erlanggal <erlangga.laimena@imslogistics.co.id>
2019-02-21 22:36:43 +0700

    Exercise 2.5

---
+++ Exercise2.5/Exercise.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	//TASK
+	//Create a variable of type string using a raw string literal. Print it.
+	x := `this is raw string and 
+	"this is string"`
+	fmt.Println(x)
+}
