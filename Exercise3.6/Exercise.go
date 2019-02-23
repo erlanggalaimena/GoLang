628cf40ddd463f202b3e23811392d76dcb092346
erlanggal <erlangga.laimena@imslogistics.co.id>
2019-02-23 22:54:06 +0700

    Exercise 3.6

---
+++ Exercise3.6/Exercise.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Task
+	//Create a program that shows the “if statement” in action.
+	x := 24
+	if x > 30 {
+		fmt.Println("Nilai x lebih besar dari 30")
+	} else {
+		fmt.Println("nilai x lebih kecil sama dengan 30")
+	}
+}
