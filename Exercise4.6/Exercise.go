b87e0ad27596cfd01928af7e93d16aa297372c43
erlanggal <erlangga.laimena@imslogistics.co.id>
2019-03-06 09:53:08 +0700

    Exercise 4.6

---
+++ Exercise4.6/Exercise.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+func main() {
+	//TASK
+	//Create a slice to store the names of all of the states in the United
+	//States of America. What is the length of your slice? What is the capacity?
+	//Print out all of the values, along with their index position in the slice,
+	//without using the range clause. Here is a list of the states:
+
+	//` Alabama`, ` Alaska`, ` Arizona`, ` Arkansas`, ` California`, ` Colorado`,
+	//` Connecticut`, ` Delaware`, ` Florida`, ` Georgia`, ` Hawaii`, ` Idaho`,
+	//` Illinois`, ` Indiana`, ` Iowa`, ` Kansas`, ` Kentucky`, ` Louisiana`,
+	//` Maine`, ` Maryland`, ` Massachusetts`, ` Michigan`, ` Minnesota`,
+	//` Mississippi`, ` Missouri`, ` Montana`, ` Nebraska`, ` Nevada`,
+	//` New Hampshire`, ` New Jersey`, ` New Mexico`, ` New York`,
+	//` North Carolina`, ` North Dakota`, ` Ohio`, ` Oklahoma`, ` Oregon`,
+	//` Pennsylvania`, ` Rhode Island`, ` South Carolina`, ` South Dakota`,
+	//` Tennessee`, ` Texas`, ` Utah`, ` Vermont`, ` Virginia`, ` Washington`,
+	//` West Virginia`, ` Wisconsin`, ` Wyoming`,
+
+	x := make([]string, 50, 100)
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
+
+	for i := 0; i < len(x); i++ {
+		x[i] = fmt.Sprintf("%d", i)
+	}
+
+	x = append(
+		x,
+		`Alabama`, `Alaska`, `Arizona`, `Arkansas`, `California`, `Colorado`,
+		`Connecticut`, `Delaware`, `Florida`, `Georgia`, `Hawaii`, `Idaho`,
+		`Illinois`, `Indiana`, `Iowa`, `Kansas`, `Kentucky`, `Louisiana`,
+		`Maine`, `Maryland`, `Massachusetts`, `Michigan`, `Minnesota`,
+		`Mississippi`, `Missouri`, `Montana`, `Nebraska`, `Nevada`,
+		`New Hampshire`, `New Jersey`, `New Mexico`, `New York`, `North Carolina`,
+		`North Dakota`, `Ohio`, `Oklahoma`, `Oregon`, `Pennsylvania`,
+		`Rhode Island`, `South Carolina`, `South Dakota`, `Tennessee`, `Texas`,
+		`Utah`, `Vermont`, `Virginia`, `Washington`, `West Virginia`,
+		`Wisconsin`, `Wyoming`)
+
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
+
+	for i := 0; i < len(x); i++ {
+		fmt.Printf("%d => %s\n", i, x[i])
+	}
+}
