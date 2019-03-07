156ed9aedce5ae43157ae7e464ce3758b435a591
erlanggal <erlangga.laimena@imslogistics.co.id>
2019-03-07 23:03:47 +0700

    Anonymous function and function expression

--- Function-GoLang-part-1/Index.go
+++ Function-GoLang-part-1/Index.go
@@ -33,6 +33,29 @@ func main() {
 	//another way to pass parameter into variadic function
 	sum3 := sumNumbers(2, 4, 6, 8)
 	fmt.Println(sum3)
+	fmt.Println("------------------------")
+
+	//Anonymous function
+	func() {
+		fmt.Println("Called from Anonymous Function")
+	}()
+
+	func(input int) {
+		fmt.Println(input, "was called from Anonymous Function")
+	}(10)
+	fmt.Println("------------------------")
+
+	//func expression
+	a := func() {
+		fmt.Println("This value is generate by function expression")
+	}
+
+	b := func(input int) {
+		fmt.Println(input, "was called from function expression")
+	}
+
+	a()
+	b(24)
 }
 
 func foo() {
