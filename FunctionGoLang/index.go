b86a1a39f9087d7f2cbc59cdec7d9592078f8771
erlanggal <erlangga.laimena@imslogistics.co.id>
2019-03-07 19:46:19 +0700

    learn how to pass slice/array type to parameter function with type variadic

--- FunctionGoLang/index.go
+++ FunctionGoLang/index.go
@@ -18,6 +18,21 @@ func main() {
 
 	sum := sumNumbers(1, 2, 3, 4, 5, 6, 7, 8, 9)
 	fmt.Println(sum)
+	fmt.Println("------------------------")
+
+	//unfurling slice
+	xi := []int{1, 3, 5, 7, 9}
+	sum2 := sumNumbers(xi...)
+	fmt.Println(sum2)
+	fmt.Println("------------------------")
+
+	//passing zero paramater to function that has parameter
+	fmt.Println(sumNumbers())
+	fmt.Println("------------------------")
+
+	//another way to pass parameter into variadic function
+	sum3 := sumNumbers(2, 4, 6, 8)
+	fmt.Println(sum3)
 }
 
 func foo() {
