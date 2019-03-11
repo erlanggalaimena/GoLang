bf22f2335df05f5dc178bf6ad12c618eced6e950
erlanggal <erlangga.laimena@imslogistics.co.id>
2019-03-11 08:30:33 +0700

    Exercise 6.7 and some change

--- Exercise6.6/Exercise.go
+++ Exercise6.6/Exercise.go
@@ -8,20 +8,7 @@ import "fmt"
 */
 
 func main() {
-	x := func(input string) string {
-		return fmt.Sprintf("My name is %s", input)
-	}("Erlangga Laimena")
-
-	y := func(numbers ...int) int {
-		total := 0
-		for _, v := range numbers {
-			total += v
-		}
-		return total
-	}
-
-	arrNumbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-
-	fmt.Println(x)
-	fmt.Println(y(arrNumbers...))
+	func(input string) {
+		fmt.Println("Some input", input)
+	}("Erlangga")
 }
---
+++ Exercise6.7/Exercise.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+/*
+	Assign a func to a variable, then call that func
+*/
+
+func main() {
+	x := func(input string) string {
+		return fmt.Sprintf("My name is %s", input)
+	}("Erlangga Laimena")
+
+	y := func(numbers ...int) int {
+		total := 0
+		for _, v := range numbers {
+			total += v
+		}
+		return total
+	}
+
+	arrNumbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+	fmt.Println(x)
+	fmt.Println(y(arrNumbers...))
+}
